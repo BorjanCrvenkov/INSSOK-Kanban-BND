ea8e0f27b1daa3af0c794213e4eaf107c320d786
Gakovski <david_gakovski@outlook.com>
2023-02-03 19:42:12 +0100

    user resource

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,11 +6,13 @@ namespace App\Providers;
 use App\Models\Board;
 use App\Models\Column;
 use App\Models\Task;
+use App\Models\User;
 use App\Models\Watches;
 use App\Models\Workspace;
 use App\Policies\BoardPolicy;
 use App\Policies\ColumnPolicy;
 use App\Policies\TaskPolicy;
+use App\Policies\UserPolicy;
 use App\Policies\WatchesPolicy;
 use App\Policies\WorkspacePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -28,6 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         Column::class    => ColumnPolicy::class,
         Task::class      => TaskPolicy::class,
         Watches::class   => WatchesPolicy::class,
+        User::class   => UserPolicy::class,
     ];
 
     /**
