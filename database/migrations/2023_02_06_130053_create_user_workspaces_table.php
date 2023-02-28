2fee9735c38fb631ae9e5fcb121a4c70f937bae8
Gakovski <david_gakovski@outlook.com>
2023-02-28 14:57:55 +0100

    user-workspaces migrations fix

--- database/migrations/2023_02_06_130053_create_user_workspaces_table.php
+++ database/migrations/2023_02_06_130053_create_user_workspaces_table.php
@@ -16,11 +16,9 @@ return new class extends Migration
     {
         Schema::create('user_workspaces', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('workspace_id');
             $table->string('access_type')->default(UserWorkspaceAccessTypeEnum::ADMIN->value);
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreign('workspace_id')->references('id')->on('workspaces')->cascadeOnDelete();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('workspace_id')->references('id')->on('workspaces')->cascadeOnDelete();
             $table->timestamps();
         });
     }
