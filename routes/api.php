878ad212511c37c499a004c511e6101495d24fc7
root <root@DESKTOP-LR242BL.localdomain>
2023-04-21 14:45:13 +0200

    Add the register route

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->controller(AuthenticationController::class)->group(function () {
     Route::post('/login', 'login')->name('login');
+    Route::post('/register', 'register')->name('register');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', 'logout')->name('logout');
